8a11bc8830da1f790c2ccdf43c830dda42cb3025
JackTrusler <jtrus93@gmail.com>
2025-05-20 18:20:34 -0400

    Add example caddyfile from jsonbournesupremacy

---
+++ nvim/nvim-lite/lua/styles.lua
@@ -0,0 +1,11 @@
+--[[
+    ________
+   | |____| |
+   | styles |
+   |  (__)  |
+   |________|
+]]
+
+require("tokyodark").setup()
+vim.cmd [[colorscheme tokyodark]]
+vim.cmd [[highlight CursorLine guibg=#383838]]
