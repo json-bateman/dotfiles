1aa581e7979d3bee193483615a2e02ce54315cbf
Jack Trusler <jtrus93@gmail.com>
2025-07-04 09:43:02 -0400

    🦈

---
+++ .wezterm.lua
@@ -0,0 +1,61 @@
+local wezterm = require("wezterm")
+
+return {
+    term = "tmux-256color",
+    font = wezterm.font_with_fallback({
+        "JetBrains Mono",
+        "Fira Code",
+        "Noto Color Emoji",
+    }),
+    color_scheme = "gruvbox_material_dark_hard",
+    font_size = 12.5,
+    enable_tab_bar = false,
+    window_background_opacity = 0.95,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
+    line_height = 1.0,
+    cell_width = 1.0,
+    adjust_window_size_when_changing_font_size = false,
+    initial_rows = 40,
+    initial_cols = 120,
+    macos_window_background_blur = 10,
+
+    color_schemes = {
+        ["gruvbox_material_dark_hard"] = {
+            foreground = "#D4BE98",
+            background = "#1D2021",
+            cursor_bg = "#D4BE98",
+            cursor_border = "#D4BE98",
+            cursor_fg = "#1D2021",
+            selection_bg = "#D4BE98",
+            selection_fg = "#3C3836",
+
+            ansi = { "#1d2021", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+            brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" }
+        },
+        ["gruvbox_material_dark_medium"] = {
+        },
+        ["gruvbox_material_dark_soft"] = {
+        },
+        ["gruvbox_material_light_hard"] = {
+            foreground = "#454735",
+            background = "#F9F5D7",
+            cursor_bg = "#654735",
+            cursor_border = "#654735",
+            cursor_fg = "#F9F5D7",
+            selection_bg = "#F3EAC7",
+            selection_fg = "#4F3829",
+
+            ansi = { "#1d2021", "#ea6962", "#79740e", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+            brights = { "#acaea5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" }
+        },
+        ["gruvbox_material_light_medium"] = {
+        },
+        ["gruvbox_material_light_soft"] = {
+        },
+    },
+}
