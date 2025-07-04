82aacb190cc241c2e5af7b73519c73622394ef7d
Jack Trusler <jtrus93@gmail.com>
2025-07-04 09:17:59 -0400

    🦈

--- nvim/lua/plugins/500-words.lua
+++
@@ -1,3 +0,0 @@
-return {
-    dir = ("/Users/jack/Coding/nvim-plugins/500-words")
-}
--- nvim/lua/plugins/512-words.lua
+++
@@ -1,9 +0,0 @@
-return {
-	dir = "~/Coding/512-words",
-	config = function()
-		require("512-words").setup({
-			storage_directory = "~",
-			file_extension = ".txt",
-		})
-	end,
-}
