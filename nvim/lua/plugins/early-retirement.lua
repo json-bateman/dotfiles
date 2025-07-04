17fc6d6f72912141d15a06b2b75d3c8aef00c312
Jack Trusler <jtrus93@gmail.com>
2025-07-04 09:17:59 -0400

    🦈

--- nvim/lua/plugins/early-retirement.lua
+++
@@ -1,6 +0,0 @@
--- Retire buffers after -- default 20 minutes -- of inactivity
-return {
-    "chrisgrieser/nvim-early-retirement",
-    config = true,
-    event = "VeryLazy",
-}
--- nvim/lua/plugins/flote.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"JellyApple102/flote.nvim",
-	config = function()
-		require("flote").setup({})
-	end,
-}
