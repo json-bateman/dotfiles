61f60cc667c7971bed6916a63abf64c46502e204
Jack Trusler <jtrus93@gmail.com>
2025-07-07 19:12:16 -0400

    🐸

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,11 @@
 -- Bootstrap Lazy, the plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable", -- latest stable release
 		lazypath,
 	})
