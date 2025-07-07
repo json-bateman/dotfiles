b08e6a8b20074db5f00fd47e842b6c6afedf4565
jacktrusler <jtrus93@gmail.com>
2025-07-07 09:42:50 -0400

    🦁

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,32 +1,45 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            log_level = vim.log.levels.DEBUG,
-            format_on_save = {
-                -- Fallback to LSP autoformatting if conform formatting doesn't have the appropriate formatter
-                lsp_format = "fallback",
-            },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                typescript = {
-                    "deno_fmt",
-                    "prettierd",
-                    "prettier",
-                    lsp_format = "fallback",
-                    stop_after_first = true,
-                },
-                typescriptreact = {
-                    "deno_fmt",
-                    "prettierd",
-                    "prettier",
-                    lsp_format = "fallback",
-                    stop_after_first = true,
-                },
-            },
-        })
-    end,
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			log_level = vim.log.levels.DEBUG,
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+			-- Templ and Html-lsp fight when saving a file. This disables html-lsp inside a templ file.
+			formatters_by_ft = {
+				lua = { "stylua" },
+				rust = { "rustfmt" },
+				-- Conform will run the first available formatter
+				typescript = {
+					"deno_fmt",
+					"prettierd",
+					"prettier",
+					lsp_format = "fallback",
+					stop_after_first = true,
+				},
+				typescriptreact = {
+					"deno_fmt",
+					"prettierd",
+					"prettier",
+					lsp_format = "fallback",
+					stop_after_first = true,
+				},
+			},
+		})
+
+		-- Templ and Html-lsp fight when saving a file. This disables html-lsp in templ files only
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				local bufnr = args.buf
+				-- Only for templ buffers!
+				if vim.bo[bufnr].filetype == "templ" and client and client.name == "html" then
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end
+			end,
+		})
+	end,
 }
--- nvim/lua/styles.lua
+++ nvim/lua/styles.lua
@@ -9,59 +9,59 @@
 
 -- Default options:
 require("gruvbox").setup({
-    terminal_colors = true, -- add neovim terminal colors
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-    },
-    inverse = false,   -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
+	terminal_colors = true, -- add neovim terminal colors
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		emphasis = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	inverse = false, -- invert background for search, diffs, statuslines and errors
+	contrast = "hard", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
 })
 require("catppuccin").setup({
-    flavour = "auto", -- latte, frappe, macchiato, mocha
-    background = {    -- :h background
-        light = "latte",
-        dark = "macchiato",
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false,            -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false,              -- Force no italic
-    no_bold = false,                -- Force no bold
-    no_underline = false,           -- Force no underline
-    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" },    -- Change the style of comments
-        conditionals = { "italic" },
-        -- ... check docs for all options
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    },
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-        -- https://github.com/catppuccin/nvim#integrations
-    },
+	flavour = "auto", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "macchiato",
+	},
+	transparent_background = false, -- disables setting the background color.
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false, -- dims the background color of inactive window
+		shade = "dark",
+		percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	},
+	no_italic = false, -- Force no italic
+	no_bold = false, -- Force no bold
+	no_underline = false, -- Force no underline
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+		-- ... check docs for all options
+		-- miscs = {}, -- Uncomment to turn off hard-coded styles
+	},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = false,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+		-- https://github.com/catppuccin/nvim#integrations
+	},
 })
 
 -- local cache_path = vim.fn.stdpath("cache") .. "/nvim_theme"
