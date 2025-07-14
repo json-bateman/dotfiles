0d9fa96ffc1ba624b240708d90cb97d7252930b5
Jack Trusler <jtrus93@gmail.com>
2025-07-13 22:13:54 -0400

    lsp stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,11 @@ return {
 	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
+		opts = {
+			settings = {
+				expose_as_code_action = "all",
+			},
+		},
 		cond = function()
 			return not (vim.fn.filereadable("deno.json") == 1 or vim.fn.filereadable("deno.jsonc") == 1)
 		end,
@@ -56,37 +60,50 @@ return {
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
-					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("<leader>ca", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
 					map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the definition of the word under your cursor.
-					map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("<leader>gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("<leader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+					map("<leader>go", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+					map(
+						"<leader>gw",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"Open Workspace Symbols"
+					)
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+					map("<leader>gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+
+					-- Autocommand for adding missing imports
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+						callback = function()
+							vim.cmd("TSToolsAddMissingImports")
+							vim.cmd("TSToolsOrganizeImports")
+						end,
+					})
 
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					-- ---@return boolean
@@ -159,6 +176,7 @@ return {
 
 				html = {},
 				cssls = {},
+				jsonls = {},
 				tailwindcss = {
 					filetypes = {
 						"templ",
