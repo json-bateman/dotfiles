6f83fe62a3e0687e022921931a01920c15f346e9
Jack Trusler <jtrus93@gmail.com>
2025-09-04 13:02:34 -0400

    messing around, made a fib script for no reason

---
+++ scripts/fibonacci/fib.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+const MAX = 1_000_000
+
+func main() {
+	n := flag.Int("n", 10, "the Fibonacci number to calculate")
+	flag.Parse()
+
+	result, err := fibonacciArray(*n)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("fib(%d) = %d\n", *n, result)
+}
+
+func fibonacciArray(n int) ([]int, error) {
+	p := message.NewPrinter(language.English)
+	if n <= 0 {
+		return nil, errors.New("must be a positive number")
+	}
+	if n >= MAX {
+		return nil, errors.New(p.Sprintf("must be a number less than %d", MAX))
+	}
+
+	seq := make([]int, n)
+	if n == 1 {
+		seq[0] = 0
+		return seq, nil
+	}
+	if n == 2 {
+		seq[0] = 0
+		seq[1] = 1
+		return seq, nil
+	}
+
+	seq[0] = 0
+	seq[1] = 1
+	for i := 2; i < n; i++ {
+		seq[i] = seq[i-1] + seq[i-2]
+	}
+	return seq[:n], nil
+}
