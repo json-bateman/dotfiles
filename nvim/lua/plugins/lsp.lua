2c627d1b33bc89f53fae90551c93c6b8ea944205
Jack Trusler <jtrus93@gmail.com>
2025-10-27 15:09:56 -0400

    stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -166,10 +166,10 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ts_ls = {},
 
+				pyright = {},
 				html = {},
 				cssls = {},
 				css_variables = {
