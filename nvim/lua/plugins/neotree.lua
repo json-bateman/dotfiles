97f3e29536060464c71e42de1506ece2be1054e8
jacktrusler <jtrus93@gmail.com>
2026-02-10 16:01:34 -0500

    styling changes to neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,6 +6,26 @@ return {
 		{ "<leader>,", "<CMD>Neotree toggle buffers<CR>", desc = "Neotree Buffers" },
 		{ "<leader>.", "<CMD>Neotree toggle reveal_force_cwd<CR>", desc = "Neotree Toggle" },
 	},
+	opts = {
+		default_component_configs = {
+			git_status = {
+				symbols = {
+					added = "✚",
+					modified = "",
+					deleted = "✖",
+					renamed = "󰁕",
+					untracked = "",
+					ignored = "",
+					unstaged = "",
+					staged = "",
+					conflict = "",
+				},
+			},
+		},
+		window = {
+			width = 25,
+		},
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
