c2e6b411b5fe1c0afa2264b1e585cd488eeb35a6
Jack Trusler <jtrus93@gmail.com>
2026-08-03 19:40:17 -0400

    consolidate common.nix across mac/linux

--- nix/home/linux.nix
+++ nix/home/linux.nix
@@ -3,11 +3,5 @@
 {
   imports = [ ./common.nix ];
 
-  programs.zsh = {
-    oh-my-zsh.theme = "murilasso";
-    initContent = ''
-      export PATH="$HOME/dotfiles/scripts:$PATH"
-      export PATH="/usr/local/bin:$PATH"
-    '';
-  };
+  # Linux-shared settings go here. zsh (theme + PATHs) now lives in common.nix.
 }
--- nix/home/mac/home.nix
+++ nix/home/mac/home.nix
@@ -6,25 +6,4 @@
   home.username = "jack";
   home.homeDirectory = "/Users/jack";
   home.stateVersion = "26.05";
-
-  programs.zsh = {
-    oh-my-zsh.theme = "robbyrussell";
-    initContent = ''
-      export PATH="$HOME/go/bin:$PATH"
-      export PATH="/opt/homebrew/bin:$PATH"
-      export PATH="/opt/homebrew/opt/sqlite/bin:$PATH"
-
-      export PNPM_HOME="$HOME/Library/pnpm"
-      case ":$PATH:" in
-        *":$PNPM_HOME/bin:"*) ;;
-        *) export PATH="$PNPM_HOME/bin:$PATH" ;;
-      esac
-
-      setopt GLOB_DOTS EXTENDED_HISTORY
-
-      eval "$(direnv hook zsh)"
-      [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-      [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
-    '';
-  };
 }
