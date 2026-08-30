b9959bee7045f2d771b69c9424267854cb34e24e
Jack Trusler <jtrus93@gmail.com>
2026-08-30 10:55:30 -0400

    hyprlanding

--- nix/home/nixos/home.nix
+++ nix/home/nixos/home.nix
@@ -11,9 +11,70 @@
     spotify
   ];
 
-  dconf.settings = {
-    "org/gnome/desktop/input-sources" = {
-      xkb-options = [ "caps:swapescape" ];
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      monitor = [ ",preferred,auto,1" ];
+
+      input = {
+        kb_layout  = "us";
+        kb_options = "caps:swapescape";
+      };
+
+      general = {
+        gaps_in     = 5;
+        gaps_out    = 10;
+        border_size = 2;
+      };
+
+      "exec-once" = [
+        "waybar"
+        "swaybg -c '#1e1e2e'"
+        "hyprpolkitagent"
+      ];
+
+      "$terminal" = "wezterm";
+      "$mainMod"  = "SUPER";
+
+      bind = [
+        "$mainMod, Return, exec, $terminal"
+        "$mainMod, Q, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, V, togglefloating"
+        "$mainMod, F, fullscreen"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+      ];
+    };
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings.mainBar = {
+      layer    = "top";
+      position = "top";
+      height   = 30;
+
+      modules-left   = [ "hyprland/workspaces" ];
+      modules-center = [ "clock" ];
+      modules-right  = [ "pulseaudio" "battery" "tray" ];
+
+      "hyprland/workspaces" = { };
+      clock                 = { format = "{:%H:%M  %a %b %d}"; };
+      pulseaudio            = { format = "{volume}% {icon}"; format-muted = "muted"; };
+      battery               = { format = "{capacity}% {icon}"; };
+      tray                  = { icon-size = 16; };
     };
   };
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -76,28 +76,7 @@ return {
 				pyright = {},
 				html = {},
 				cssls = {},
-				css_variables = {
-					settings = {
-						cssVariables = {
-							blacklistFolders = {
-								"**/.cache",
-								"**/.DS_Store",
-								"**/.git",
-								"**/.hg",
-								"**/.next",
-								"**/.svn",
-								"**/bower_components",
-								"**/CVS",
-								"**/dist",
-								"**/node_modules",
-								"**/tests",
-								"**/tmp",
-							},
-							lookupFiles = { "**/*.less", "**/*.scss", "**/*.sass", "**/*.css" },
-						},
-					},
-				},
-
+				css_variables = {},
 				jsonls = {},
 				templ = {},
 				gopls = {},
